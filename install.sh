56da0f0b3d7ce48a598aeb4b1370cdf9a0688d37
Gustavo Franco <gustavocfranco@gmail.com>
2014-06-27 18:39:24 -0300

    Add links to git configuration

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+echo "Configuring vim"
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vimrc ~/.vimrc
+
+echo "Configuring Git"
+ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/git/gitglobalignore ~/.gitglobalignore
