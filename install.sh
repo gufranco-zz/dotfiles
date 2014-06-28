a40a481c45f072d8c1ed1fb9bb7e1932b4858480
Gustavo Franco <gustavocfranco@gmail.com>
2014-06-28 11:48:48 -0300

    Add oh-my-zsh install script and zshrc

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ ln -s ~/.dotfiles/vimrc ~/.vimrc
 echo "Configuring Git"
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/gitglobalignore ~/.gitglobalignore
+
+echo "Configuring oh-my-zsh"
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/oh-my-zsh ~/.oh-my-zsh
